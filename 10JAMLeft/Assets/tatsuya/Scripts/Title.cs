664cbb1114571545e76045dae31c72e250eebb9a
estrela <daisuke.0530.otk@gmail.com>
2021-09-16 13:11:07 +0900

    石油王スクリプト修正

--- 10JAMLeft/Assets/tatsuya/Scripts/Title.cs
+++ 10JAMLeft/Assets/tatsuya/Scripts/Title.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Title : MonoBehaviour
 {
-    private SceneChange change;
+    private SceneChanger change;
     // Start is called before the first frame update
     void Start()
     {
-        change = GetCompornent<SceneChanger>();        
+        change = GetComponent<SceneChanger>();        
     }
 
     // Update is called once per frame
