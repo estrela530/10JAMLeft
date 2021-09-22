0d071a821e223616bff88c1aa2a3db2b50dfe884
Cthugha <tatsusato0519@gmail.com>
2021-09-22 14:41:24 +0900

    シーン遷移用フェードの作成(未完）

---
+++ 10JAMLeft/Assets/tatsuya/Scripts/Fade.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField] private float fadeSpeed = 0.0f;
+
+    public string NextChangerSceneName;
+    private float red, green, blue, alfa;//パネルの色、透明度
+    private Image fadePanelImage;
+    private bool isStart = true;
+    private SceneChanger sceneChange;
+    private Character player;
+    //private Goal goal;
+    private bool isLoadScene = false;
+
+    private bool isSound;
+
+    private void Start()
+    {
+        fadePanelImage = GetComponent<Image>();
+        red = fadePanelImage.color.r;
+        green = fadePanelImage.color.g;
+        blue = fadePanelImage.color.b;
+        alfa = fadePanelImage.color.a;
+        sceneChange = FindObjectOfType<SceneChanger>();
+        //goal = FindObjectOfType<Goal>();
+        isSound = false;
+    }
+
+    private void Update()
+    {
+        //if (SceneSave.Instance.IsNowScene == "Title")
+        //{
+        //    if ()
+        //    {
+        //        StartFadeOut();
+
+        //        if (isLoadScene)
+        //        {
+        //            sceneChange.ChangeScene("StageSelect");
+        //        }
+        //    }
+        //}
+
+        if (SceneSave.Instance.IsNowScene.Contains("Stage"))
+        {
+            //if (player.IsDeadFlag)
+            //{
+            //    if (!isSound)
+            //    {
+            //        SoundManager.Instance.PlaySeByName("プレイヤーが死んだとき");
+            //        isSound = true;
+            //    }
+            //    StartFadeOut();
+
+            //    if (isLoadScene)
+            //    {
+            //        SoundManager.Instance.StopSe();
+            //        sceneChange.ChangeScene("GameOver");
+            //    }
+            //}
+
+            //if (goal.IsGoal)
+            //{
+            //    StartFadeOut();
+
+            //    if (isLoadScene)
+            //    {
+            //        SoundManager.Instance.StopSe();
+            //        sceneChange.ChangeScene("Clear");
+            //    }
+            //}
+        }
+    }
+
+    private void StartFadeIn()
+    {
+        if (isStart)
+        {
+            fadePanelImage.enabled = true;
+            isStart = false;
+        }
+
+        alfa -= fadeSpeed;
+        SetAlpha();
+        if (alfa <= 0)
+        {
+            fadePanelImage.enabled = false;//パネルの表示をオフにする
+            isLoadScene = true;
+        }
+    }
+
+    private void StartFadeOut()
+    {
+        if (isStart)
+        {
+            alfa = 0;
+            SetAlpha();
+            isStart = false;
+        }
+
+        fadePanelImage.enabled = true;//パネルの表示をオンにする
+        alfa += fadeSpeed;
+        SetAlpha();
+        if (alfa >= 1)
+        {
+            isLoadScene = true;
+        }
+    }
+
+    private void SetAlpha()
+    {
+        fadePanelImage.color = new Color(red, green, blue, alfa);
+    }
+}
